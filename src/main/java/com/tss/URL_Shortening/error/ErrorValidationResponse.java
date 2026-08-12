96ab2dc90ec3a495524db1c75a5da2db62583b23
Prince Kakadiya <prince.kakadiya@tssconsultancy.com>
2026-08-12 10:54:38 +0530

    fix send mail and code for handling global exception

---
+++ src/main/java/com/tss/URL_Shortening/error/ErrorValidationResponse.java
@@ -0,0 +1,16 @@
+package com.tss.URL_Shortening.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+public class ErrorValidationResponse {
+    private Integer code;
+    private String errorMessage;
+    private LocalDateTime errorTime;
+    private Map<String,String> errors;
+}
